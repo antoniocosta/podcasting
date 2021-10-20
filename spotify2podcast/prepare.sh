e0b1868890b6c65599cf096d18f42ddc277277a4
abmc <abmc@vmi602670.contaboserver.net>
2021-10-20 18:01:53 +0200

    Minor fixes and optimizations

--- spotify2podcast/prepare.sh
+++ spotify2podcast/prepare.sh
@@ -26,32 +26,40 @@ source $2 # Include the episode config file passed as argument
 
 echo "Starting..."
 
-if [ ! -f $ARCHIVE_DIR'/'$EP_FILE ]; then
-    echo "Mp3 episode file $EP_FILE not found in $ARCHIVE_DIR"
-    echo "Exiting!"
-    exit;
-fi
+if [ -f $ARCHIVE_DIR'/'$EP_FILE ]; then
+    EP_FULLPATH=$ARCHIVE_DIR'/'$EP_FILE
+elif [ ! -f $ARCHIVE_DIR'/'$EP_SUBDIR'/'$EP_FILE ]; then
+    EP_FULLPATH=$ARCHIVE_DIR'/'$EP_SUBDIR'/'$EP_FILE
 
-# 1. Move merged mp3 from download subdir to one dir up
-mv $ARCHIVE_DIR'/'$EP_FILE $ARCHIVE_DIR # Move ep mp3 file one dir up (to the main download folder)
+    # 1. Move merged mp3 from download subdir to one dir up
+    mv $EP_FULLPATH $ARCHIVE_DIR # Move ep mp3 file one dir up (to the main download folder)
+
+else
+    echo "Mp3 episode file $EP_FILE not found in $ARCHIVE_DIR or $EP_SUBDIR subdir"
+    echo "Exiting!"
+    exit
+fi
+echo "Episode mp3 found: $EP_FULLPATH"
 
 # 2. Clean up tmp files from download subdir
 echo "Cleaning up..."
-rm $ARCHIVE_DIR'/.spotdl-cache' # spot-dl auth file
-rm $ARCHIVE_DIR'/tmp.txt' # The ffmpeg temp merge file
+rm -f $ARCHIVE_DIR'/'$EP_SUBDIR'/.spotdl-cache' # Remove spot-dl auth file (dont show error if doesnt exist)
+rm -f $ARCHIVE_DIR'/'$EP_SUBDIR'/tmp.txt' # Remove ffmpeg temp merge file (dont show error if it doenst exist)
 
 # 3. Generate json from merged mp3 and episode config file
 function print_json {
     local id=${EP_FILE%'.'$RSS_AUDIO_FORMAT} # Same as mp3 filename (minus extension)
     local title="$RSS_TITLE $ID3_TITLE"
-    local timestamp=$(date -r "$EP_FILE" "+%s")
+    local timestamp=$(date -r "$EP_FULLPATH" "+%s")
     local webpage_url=$RSS_LINK'/episode/'$EP_NUM
     # convert band names from m3u to / delimited single line. Ignore intro and outro (any line without ' -')
-    local description=$(cat "$M3U_FILE" | sed '/ -/!d' | sed 's/ -.*//' | sed -e :a -e '$!N; s/\n/ \/ /; ta')
+    local description=$EP_DESCRIPTION" In this episode: "$(cat "$M3U_FILE" | sed '/ -/!d' | sed 's/ -.*//' | sed -e :a -e '$!N; s/\n/ \/ /; ta')
     local json_fmt='{"id": "%s", "title": "%s", "timestamp": %s, "webpage_url": "%s", "description": "%s"}\n'
     printf "$json_fmt" "$id" "$title" "$timestamp" "$webpage_url" "$description"
 }
 echo "Generating JSON..."
+M3U_FILE=$(find $ARCHIVE_DIR'/'$EP_SUBDIR -type f -name "*.m3u") # Works but only if find command will return exactly 1 file
+echo "Episode m3u found: $M3U_FILE"
 JSON_FILE=${EP_FILE%.mp3}.info.json # json filename from mp3 file
 print_json > $ARCHIVE_DIR'/'$JSON_FILE
 
